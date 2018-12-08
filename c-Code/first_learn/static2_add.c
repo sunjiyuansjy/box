17b4f73b3e2d0c73be9d47e315d980a53a29811a
sunjiyuansjy <sunjiyuansjy@163.com>
2018-12-09 07:37:11 +0800

    删除远程练习

--- c-Code/first_learn/static2_add.c
+++
@@ -1,7 +0,0 @@
-//代码1
-//static2_add.c
-//int g_val=2018;
-
-//代码2
-//add.c
-static int g_val=2018;
--- c-Code/first_learn/static3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-//代码1
-int main()
-{
-	printf("%d\n",Add(2,3));
-	return 0;
-}
