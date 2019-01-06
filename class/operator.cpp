d5bb87532fa3aa116a8158bbca23cc3d4df1b64d
sunjiyuansjy <sunjiyuansjy@163.com>
2019-01-07 07:30:05 +0800

    添加运算符重载

--- class/operator.cpp
+++ class/operator.cpp
@@ -69,3 +69,30 @@ void Test()
 	Date d2(2018, 9, 27);
 	cout<<(d1 == d2)<<endl;
 }
+class Date
+{
+
+	public:
+		Date(int year = 1900, int month = 1, int day = 1)
+		{
+
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+	private:
+		int _year;
+		int _month;
+		int _day;
+};
+int main()
+{
+
+	Date d1;
+	Date d2(2018，10， 1);
+	// 这里d1调用的编译器生成operator=完成拷贝，d2和d1的值也是一样的。
+	 d1 = d2；
+	 return 0;
+	// }
+}
+
